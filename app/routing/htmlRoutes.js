d7e04e56149df63d9bc7ff00630bf6b57bd3a2bc
Edward <edwardgretsch@gmail.com>
2018-07-30 05:32:01 -0700

    update 2

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.js"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-    app.get("/survay", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.js"));
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 }
